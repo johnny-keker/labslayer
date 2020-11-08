7c18c5ce3249ba9026fd1e1174e7ac900fdac3c9
johnny-keker <giomail.iv@gmail.com>
2020-11-08 23:23:45 +0300

    modifiyng lava model matrix, wasd wip

--- src/js/index.js
+++ src/js/index.js
@@ -13,6 +13,12 @@ let bufferInfo;
 let phase = 0;
 let plane;
 let rotations = [0, 0, 0];
+let playerX = 0;
+let playerY = 0;
+
+var pressedKeys = {};
+window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; }
+window.onkeydown = function(e) { pressedKeys[e.keyCode] = true; }
 
 async function main() {
   const canvas = document.createElement("CANVAS");
@@ -62,28 +68,52 @@ async function main() {
 function render(time) {
   phase += 0.05;
   twgl.resizeCanvasToDisplaySize(gl.canvas);
+
+  /* ---------------- */
   gl.clearColor(0.0, 0.0, 0.0, 1.0);
   gl.clearDepth(1.0);
   gl.enable(gl.DEPTH_TEST);
   gl.depthFunc(gl.LEQUAL);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  
+  /* ---------------- */
+
+  var lavaWidth = 1;
+  var lavaLength = 1;
+
+  // okay this boy handles the size of the lava lake. would be nice to divide its dims
+  // by sceen size to make it actually square
+  // actualy we can rotate and translate it too
+  var modelMatrix = new Float32Array([lavaWidth, 0, 0, 0, 0, 1, 0, 0, 0, 0, lavaLength, 0, 0, 0, 0, 1]);
+
+
   var projectionMatrix = new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
-  var modelViewMatrix = new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
+  
+  var viewMatrix = new Float32Array([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
 
   projectionMatrix = matrix.perspective(projectionMatrix,
     45,
     gl.canvas.clientWidth / gl.canvas.clientHeight,
     0.1,
     1000);
-  projectionMatrix = matrix.rotate(projectionMatrix, rotations)
-  modelViewMatrix = matrix.translate(modelViewMatrix, 500, -20, 0);
-  modelViewMatrix = matrix.rotate(modelViewMatrix, [-90, 0, 0]);
+  projectionMatrix = matrix.rotate(projectionMatrix, rotations);
+
+  // we need to rotate lava lake to make it horizontal and
+  // we need to move it down a bit
+  modelMatrix = matrix.rotate(modelMatrix, [-90, 0, 0]);
+  modelMatrix = matrix.translate(modelMatrix, 0, -20, 0);
+
+  // if (pressedKeys[87]) {
+  //   playerX += Math.cos(matrix.radToDeg(rotations[1]));
+  //   playerY += Math.sin(matrix.radToDeg(rotations[1]));
+  //   //modelViewMatrix = matrix.translate(modelViewMatrix, playerX, 0, playerY);
+  // }
+    
 
   const uniforms = {
     uPhase: phase,
-    uModelViewMatrix: modelViewMatrix,
+    uModelMatrix: modelMatrix,
+    uViewMatrix: viewMatrix,
     uProjectionMatrix: projectionMatrix
     //vTexcoord: plane.texcoors
   }
--- src/shaders/lava_V.glsl
+++ src/shaders/lava_V.glsl
@@ -3,12 +3,13 @@ precision mediump float;
 attribute vec4 aVertexPosition;
 
 // provided
-uniform mat4 uModelViewMatrix;
+uniform mat4 uModelMatrix;
+uniform mat4 uViewMatrix;
 uniform mat4 uProjectionMatrix;
 uniform float uPhase;
 
 void main() {
   vec4 pos = aVertexPosition;
   pos.z = 1.0 * sin(0.4 * pos.x + uPhase) * sin(0.4 * pos.y + uPhase);
-  gl_Position = uProjectionMatrix * uModelViewMatrix * pos;
+  gl_Position = uProjectionMatrix * uModelMatrix * uViewMatrix * pos;
 }
